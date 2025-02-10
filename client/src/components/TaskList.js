60202d7937c9b09c2f0add490081417c6ed2a15d
Muzammil <Muzammilsindhu2000@gmail.com>
2025-02-11 02:44:50 +0500

    TaskList.js file updated

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,17 +1,11 @@
 import React from "react";
-import { motion } from "framer-motion";
 import { FaTrash } from "react-icons/fa";
 
 const TaskList = ({ tasks, markAsComplete, deleteTask }) => {
   return (
     <ul>
       {tasks.map(({ _id, task, completed }) => (
-        <motion.li
-          key={_id}
-          initial={{ opacity: 0, y: -10 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -10 }}
-        >
+        <li key={_id}>
           <span className={completed ? "completed-task" : ""}>{task}</span>
           <div className="task-actions">
             {!completed && (
@@ -26,7 +20,7 @@ const TaskList = ({ tasks, markAsComplete, deleteTask }) => {
               <FaTrash />
             </button>
           </div>
-        </motion.li>
+        </li>
       ))}
     </ul>
   );
